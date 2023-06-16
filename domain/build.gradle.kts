f52fafa826888675277c0edd7a2a982fda539774
hseungho <hsh47607@naver.com>
2023-06-17 08:03:07 +0900

    build: add dependencies of jpa, redis, mysql

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
+    kotlin("plugin.jpa")
 }
 
 version = "0.0.1"
 
-dependencies {
+allprojects {
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
+        implementation("mysql:mysql-connector-j")
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    }
 }
