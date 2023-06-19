c49e869336faa03009b3f4a8867f7f2741b902e2
hseungho <hsh47607@naver.com>
2023-06-19 09:07:37 +0900

    Feat: Init CSS module

---
+++ css/src/main/kotlin/com/zerobase/hseungho/css/CssApplication.kt
@@ -0,0 +1,11 @@
+package com.zerobase.hseungho.css
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CssApplication
+
+fun main(args: Array<String>) {
+    runApplication<CssApplication>(*args)
+}
