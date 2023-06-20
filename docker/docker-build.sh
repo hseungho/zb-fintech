56a113ca958a3f3d3090a595003934d21d7d931a
hseungho <hsh47607@naver.com>
2023-06-20 18:36:35 +0900

    Docker: create docker build script

---
+++ docker/docker-build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Setting version
+VERSION="$0"
+
+cd ..
+./gradlew clean build -x test
+
+ROOT_PATH=`pwd`
+echo "ROOT_PATH: '$ROOT_PATH'"
+
+echo 'api Docker image build...'
+cd $ROOT_PATH/api && docker build -t fintech-api:$VERSION .
+echo 'api Docker image build... Done'
+
+echo 'consumer Docker image build...'
+cd $ROOT_PATH/consumer && docker build -t fintech-consumer:$VERSION .
+echo 'consumer Docker image build... Done'
+
+echo 'css Docker image build...'
+cd $ROOT_PATH/css && docker build -t fintech-css:$VERSION .
+echo 'css Docker image build... Done'
+
+echo 'nginx Docker image build...'
+cd $ROOT_PATH/nginx && docker build -t fintech-nginx:$VERSION .
+echo 'nginx Docker image build... Done'
