5122ded7711bdedbfcc850427a48828d1c98258a
hseungho <hsh47607@naver.com>
2023-06-20 19:04:19 +0900

    Docker: updated docker build script

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -3,24 +3,20 @@
 # Setting version
 VERSION="1.0.0"
 
-cd ..
 ./gradlew clean build -x test
 
-ROOT_PATH=`pwd`
-echo "ROOT_PATH: '$ROOT_PATH'"
-
 echo 'api Docker image build...'
-cd $ROOT_PATH/api && docker build -t fintech-api:$VERSION .
+docker build -t fintech-api:$VERSION -f api/Dockerfile .
 echo 'api Docker image build... Done'
 
 echo 'consumer Docker image build...'
-cd $ROOT_PATH/consumer && docker build -t fintech-consumer:$VERSION .
+docker build -t fintech-consumer:$VERSION -f consumer/Dockerfile .
 echo 'consumer Docker image build... Done'
 
 echo 'css Docker image build...'
-cd $ROOT_PATH/css && docker build -t fintech-css:$VERSION .
+docker build -t fintech-css:$VERSION -f css/Dockerfile .
 echo 'css Docker image build... Done'
 
 echo 'nginx Docker image build...'
-cd $ROOT_PATH/nginx && docker build -t fintech-nginx:$VERSION .
+docker build -t fintech-nginx:$VERSION -f nginx/Dockerfile .
 echo 'nginx Docker image build... Done'
